96520dc96ee485a3d0aed80090b87e68100e3875
Samuel <samuelsantxxi@gmail.com>
2021-09-11 17:18:52 -0300

    resized images

--- backend/src/controllers/PostController.js
+++ backend/src/controllers/PostController.js
@@ -1,3 +1,6 @@
+const sharp = require('sharp');
+const path = require('path');
+const fs = require('fs');
 const Post = require('../models/Post');
 
 
@@ -10,6 +13,12 @@ module.exports = {
    async store (request, response) {
     const { author, place, description, hashtags } = request.body;
     const { filename: image } = request.file;
+
+    await sharp(request.file.path)
+      .resize(500)
+      .jpeg({ quality: 70 })
+      .toFile(path.resolve(request.file.destination, 'resized', request.file.filename))
+    fs.unlinkSync(request.file.path)
     const post = await Post.create({ author, place, description, hashtags, image });    
     return response.status(201).json(post)
   }
--- backend/src/index.js
+++ backend/src/index.js
@@ -1,9 +1,11 @@
 const express = require('express');
 const mongooseConnection= require('./config/mongoose');
+const path = require('path');
 
 mongooseConnection.createConnection().then(() => console.log('DB!!'))
 const app = express();
 
+app.use('/files', express.static(path.resolve(__dirname, '..', 'uploads', 'resized')))
 app.use(require('./routes'))
 
 app.listen(3333, () => console.log('Starting server!!'))

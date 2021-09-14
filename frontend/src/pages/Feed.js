f9fee3ceb0b3e02f9c957a7c9a5c341675ac3a02
Samuel <samuelsantxxi@gmail.com>
2021-09-13 23:15:24 -0300

    handle like

--- frontend/src/pages/Feed.js
+++ frontend/src/pages/Feed.js
@@ -1,4 +1,5 @@
 import React, { Component } from "react";
+import api from '../services/api';
 
 import more from '../assets/more.svg';
 import like from '../assets/like.svg';
@@ -8,36 +9,54 @@ import send from '../assets/send.svg';
 import './Feed.css';
 
 class Feed extends Component {
+  state = {
+    feed: [],
+  }
+  
+  async componentDidMount() {
+    const response = await api.get('posts');
+
+    this.setState({ feed: response.data })
+  }
+
+  handleLike = async(id) => {
+    await api.post(`posts/${id}/like`);
+  }
+
   render() {
     return (
       <section id="post-list">
+        { this.state.feed.map(post =>
         <article>
           <header>
             <div className="user-info">
-              <span>Samuel dos Santos Batista </span>
-              <span className="place">MG</span>
+              <span>{post.author}</span>
+              <span className="place">{post.place}</span>
             </div>
 
             <img src={more} alt="Mais" />
           </header>
 
-          <img src="https://img.olhardigital.com.br/wp-content/uploads/2021/06/post3-1024x877.jpg" alt="post imagem" />
+          <img src={`http://localhost:3333/files/${post.image}`} alt="post imagem" />
         
           <footer>
             <div className="actions">
-              <img src={like} alt="like" />
+              <button onClick={() =>this.handleLike(post._id)}>
+                <img src={like} alt="like" />
+              </button>
               <img src={comment} alt="Comentário" />
               <img src={send} alt="Enviar" />
             </div>
             
-            <strong>900 curtidas</strong>
+            <strong>{post.likes} curtidas</strong>
 
             <p>
-              Um post muito massa da Omnistack
-              <span>#react #omnistack</span>
+              {post.description}
+              <span>{post.hashtags}</span>
             </p>
           </footer>
         </article>
+      )}
       </section>
     );
   }
--- frontend/src/pages/New.js
+++ frontend/src/pages/New.js
@@ -1,9 +1,74 @@
 import React, { Component } from "react";
+import api from "../services/api";
+
+import './New.css';
 
 class New extends Component {
+  state = {
+    image: null,
+    author: '',
+    place: '',
+    description: '',
+    hashtags: '',
+  }
+  handleSubmit = async e => {
+    e.preventDefault();
+    console.log(this.state)
+    const data = new FormData();
+    data.append('image', this.state.image);
+    data.append('author', this.state.author)
+    data.append('place', this.state.place);
+    data.append('description', this.state.description);
+    data.append('hashtags', this.state.hashtags);
+    await api.post('posts', data);
+    this.props.history.push('/');
+  }
+
+  handleChangeImage = e => {
+    this.setState({
+      image: e.target.files[0]
+    })
+  }
+
+  handleChange = e => {
+    this.setState({
+      [e.target.name]: e.target.value
+    })
+  }
   render() {
     return (
-      <h1>New</h1>
+      <form id="new-post">
+        <input type="file" onChange={this.handleChangeImage}/>
+        <input 
+          type="text"
+          name="author"
+          placeholder="Author do post"
+          onChange={this.handleChange}
+          value={this.state.author}
+        />
+        <input 
+          type="text"
+          name="place"
+          placeholder="Local do post"
+          onChange={this.handleChange}
+          value={this.state.place}
+        />
+        <input 
+          type="text"
+          name="description"
+          placeholder="Description do post"
+          onChange={this.handleChange}
+          value={this.state.description}
+        />
+        <input 
+          type="text"
+          name="hashtags"
+          placeholder="Hashtags do post"
+          onChange={this.handleChange}
+          value={this.state.hashtags}
+        />
+        <button type="submit" onClick={this.handleSubmit}>Enviar</button>
+      </form>
     );
   }
 }
